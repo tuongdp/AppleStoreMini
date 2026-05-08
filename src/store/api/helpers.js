8eabd211c9c4e54b6aca4b1e9ccade3f18040c66
tuongdo <tuongdp@imt-soft.com>
2026-05-08 20:39:17 +0700

    fix helpers flashsale

--- src/store/api/helpers.js
+++ src/store/api/helpers.js
@@ -2,12 +2,6 @@ export const parseProduct = (p) => ({
     ...p,
     images:
         typeof p.images === "string" ? JSON.parse(p.images) : (p.images ?? []),
-    colors:
-        typeof p.colors === "string" ? JSON.parse(p.colors) : (p.colors ?? []),
-    storage:
-        typeof p.storage === "string"
-            ? JSON.parse(p.storage)
-            : (p.storage ?? []),
     specifications:
         typeof p.specifications === "string"
             ? JSON.parse(p.specifications)
