d33829656b7746da8caa83bdfdec6681ded36b77
tuongdo <tuongdp@imt-soft.com>
2026-05-15 12:29:40 +0700

    feat: add admin shop settings page wrapper

---
+++ src/pages/admin/AdminShopSettings.jsx
@@ -0,0 +1,5 @@
+import AdminShopSettingsComponent from "@/features/admin/components/shop/AdminShopSettings";
+
+export default function AdminShopSettingsPage() {
+  return <AdminShopSettingsComponent />;
+}
